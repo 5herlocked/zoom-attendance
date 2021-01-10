b3fb3f84d3d6c3d0b9ab1446d32b5da2a401d7db
Shardul Vaidya <cam.v737@gmail.com>
2021-01-10 16:20:20 -0500

    Added in NuGet Package for CSV driver, along with the MySQL driver

---
+++ Attendance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoom_attendance
+{
+    public class Attendance
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public List<DateTime> AttendedDates { get; set; }
+        public Boolean Regular { get; set; }
+    }
+}
