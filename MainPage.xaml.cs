92f1e2218b918f0b0bbbf702021b20c766803264
Shardul Vaidya <cam.v737@gmail.com>
2021-01-10 22:36:20 -0500

    WIP: Initial UI completed, some design choices still incomplete

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,45 @@ namespace zoom_attendance
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public List<Attendance> attendees = new List<Attendance>();
+        public StorageFolder folder = null;
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Browse_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string possibleFolder = Folder_Text.Text;
+            if (!string.IsNullOrEmpty(possibleFolder))
+            {
+                folder = await StorageFolder.GetFolderFromPathAsync(possibleFolder);
+            }
+            else
+            {
+                var folderPicker = new FolderPicker
+                {
+                    SuggestedStartLocation = PickerLocationId.Desktop
+                };
+
+                folder = await folderPicker.PickSingleFolderAsync();
+            }
+
+            if (folder != null)
+            {
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                    FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                Folder_Text.Text = folder.Name;
+            }
+            else
+            {
+                Folder_Text.Text = "Invalid Folder";
+            }
+        }
+
+        private void Analyse_Logs_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
