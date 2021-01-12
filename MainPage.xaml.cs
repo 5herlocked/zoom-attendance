787edf03aff9bd0992db4e9e7c35298728f379df
swooo <liushanting@gmail.com>
2021-01-11 21:57:24 -0500

    updating with cvsHelper code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using CsvHelper;
+using System.Globalization;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,11 +26,41 @@ namespace zoom_attendance
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public StorageFolder folder = null;
         public List<Attendance> attendees = new List<Attendance>();
         public StorageFolder folder = null;
+
+        public class Foo
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Date { get; set; }
+        }
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public async void parse()
+        {
+            if (folder == null) return;
+
+            List<StorageFile> fileStream = (List<StorageFile>)await folder.GetItemsAsync();
+
+            foreach (var file in fileStream)
+            {
+                if (file.FileType.Equals("csv"))
+                {
+                    using (var reader = await file.OpenReadAsync())
+                    {
+                        using (var csv = new CsvReader((TextReader)reader, CultureInfo.InvariantCulture))
+                        {
+                            var records = csv.GetRecords<Foo>();
+                        }
+
+                    }
+                }
+            }
         }
 
         private async void Browse_Button_Click(object sender, RoutedEventArgs e)
